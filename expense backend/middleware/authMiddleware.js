3032cd8b386560ba88b05fc18b2f0f0f905f5b5a
Barathvajk <Barathvajk>
2026-02-24 07:30:54 +0530

    Updated

---
+++ expense backend/middleware/authMiddleware.js
@@ -0,0 +1,21 @@
+const jwt = require("jsonwebtoken");
+
+const protect = (req, res, next) => {
+  const header = req.headers.authorization;
+
+  if (!header) {
+    return res.status(401).json({ message: "Not authorized" });
+  }
+
+  const token = header.split(" ")[1];
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded;
+    next();
+  } catch {
+    res.status(401).json({ message: "Token invalid" });
+  }
+};
+
+module.exports = protect;
