8e234132d63db6eece22b69fae8e05df2aedac3d
Barathvajk <Barathvajk>
2026-03-12 07:42:37 +0530

    MODIFIED

--- backend/expense backend/utils/generateToken.js
+++
@@ -1,9 +0,0 @@
-const jwt = require("jsonwebtoken");
-
-const generateToken = (id) => {
-  return jwt.sign({ userId: id }, process.env.JWT_SECRET, {
-    expiresIn: "1h"
-  });
-};
-
-module.exports = generateToken;
